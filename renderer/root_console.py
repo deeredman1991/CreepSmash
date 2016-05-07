97f91e328808cb6fdf6ba19beef506e13bb3a222
deeredman1991 <deeredman1991@gmail.com>
2016-05-07 12:18:26 -0400

    updated the Json parser

--- renderer/root_console.py
+++ renderer/root_console.py
@@ -4,12 +4,8 @@ import tools.toolbox as toolbox
 import tools.libtcod.libtcodpy as libtcod
 
 def init():
-    if not os.path.isfile('settings/font.json'):
-        shutil.copyfile('settings/.defaults/font.json', 'settings/font.json')
     font_settings = toolbox.parseJson('settings/font.json')
     libtcod.console_set_custom_font('fonts/{}'.format(font_settings['Fontname']), libtcod.FONT_TYPE_GREYSCALE | font_settings['Layout'], font_settings['Width'], font_settings['Height'])
     
-    if not os.path.isfile('settings/window.json'):
-        shutil.copyfile('settings/.defaults/window.json', 'settings/window.json')
     window_settings = toolbox.parseJson('settings/window.json')
     libtcod.console_init_root(window_settings['Width'], window_settings['Height'], 'CreepSmash', window_settings['Fullscreen'])
--- tools/toolbox.py
+++ tools/toolbox.py
@@ -1,6 +1,14 @@
+import os.path
+import shutil
 import json
 
 def parseJson(filepath):
+    if not os.path.isfile(filepath):
+        defaults_filepath = filepath.split('/')
+        filename = defaults_filepath.pop(-1)
+        defaults_filepath = '{}/.defaults/{}'.format('{}'.join(defaults_filepath), filename)
+        shutil.copyfile(defaults_filepath, filepath)
+        
     with open(filepath, 'r') as infile:
         jsonData = json.load(infile) 
     return jsonData
