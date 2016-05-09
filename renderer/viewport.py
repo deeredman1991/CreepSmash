c383f05e5bd9f99b28e43ec4ad71863bab28f457
deeredman1991 <deeredman1991@gmail.com>
2016-05-09 15:55:08 -0400

    changed viewport border to grey

--- renderer/viewport.py
+++ renderer/viewport.py
@@ -7,7 +7,7 @@ class ViewportConsole(console.Console):
         for x in range(0, self.width):
             for y in range (0, self.height):
                 if x == 0 or y == 0 or x == self.width-1 or y == self.height-1:
-                    libtcod.console_put_char_ex(self.console, x, y, ' ', libtcod.black, libtcod.light_red)
+                    libtcod.console_put_char_ex(self.console, x, y, ' ', libtcod.black, libtcod.light_grey)
                 else:
                     libtcod.console_put_char(self.console, x, y, '.', libtcod.BKGND_NONE)
                     
