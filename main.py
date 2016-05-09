80b20deb24ba6e8a07a0c92dc585cc56dc315556
deeredman1991 <deeredman1991@gmail.com>
2016-05-09 15:38:45 -0400

    Added audio support and the first music file

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 import os
 import renderer.root_console as root_console
+import tools.audio_player as audio_player
 import tools.libtcod.libtcodpy as libtcod
 import tools.toolbox as toolbox
 
 
 keybindings = toolbox.parseJson('settings/keybindings')
+music = toolbox.parseJson('settings/music')
 
 print ( keybindings["KeyCodes"][ keybindings['Controls']['CamUp'] ] )
 print ( keybindings["KeyCodes"][ keybindings['Controls']['CamDown'] ] )
 
 window = root_console.RootConsole()
+audio_player = audio_player.AudioPlayer()
 
 def main():
     if not os.path.exists('saves/autosave'):
         os.makedirs('saves/autosave')
         
     while not libtcod.console_is_window_closed():
-    
+        if audio_player.music_track == None:
+            audio_player.music_track = music["WorldTheme"]
+            
         window.render_all()
         
         libtcod.console_wait_for_keypress(True)
---
+++ tools/audio_player.py
@@ -0,0 +1,76 @@
+import threading
+import time
+import tools.pyaudio.pyaudio as pyaudio
+import wave
+
+
+class AudioPlayer(object):
+    def __init__(self):
+        self._audio_file = None
+        self._music_track = None
+        self._stop_music = False
+        
+    @property
+    def music_track(self):
+        return self._music_track
+        
+    @music_track.setter
+    def music_track(self, track):
+        if track != None:
+            self.play_music(track)
+        
+    def stop_music(self):
+        self._stop_music = True
+        
+    def play_music(self, musicfile):
+        while self._music_track != None:
+            self.stop_music()
+            time.sleep(0.5)
+        self._audio_file = musicfile
+        self._music_track = self._audio_file
+        threading.Thread( target = (self._play_audio) ).start()
+        
+    def play_sound(self, soundfile):
+        self._audio_file = soundfile
+        threading.Thread( target = (self._play_audio) ).start()
+        time.sleep(0.5)
+        
+    def _play_audio(self):
+        chunk_size = 1024
+        _audio_file = self._audio_file
+        
+        wf = wave.open(_audio_file, 'rb')
+
+        p = pyaudio.PyAudio()
+        stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                        channels=wf.getnchannels(),
+                        rate=wf.getframerate(),
+                        output=True)
+                
+        data = wf.readframes(chunk_size)
+
+        while data != '':
+            if self._stop_music:
+                break
+            stream.write(data)
+            data = wf.readframes(chunk_size)
+            
+        stream.stop_stream()
+        stream.close()
+
+        p.terminate()
+        
+        if _audio_file == self._music_track:
+            if self._stop_music == True:
+                self._stop_music = False
+            self._music_track = None
+            self.playing_music = False
+            
+
+if __name__ == '__main__':
+    csAnthem = "sound/music/Creepsmash_Anthem.wav"
+    tick = "sound/tick.wav"
+
+    audio_player = AudioPlayer()
+    audio_player.play_music(csAnthem)
+    print ("Now playing Creepsmash Anthem!")
