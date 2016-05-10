2d26f3f31c84c2eb43eb88a520d39f13554a84e6
deeredman1991 <deeredman1991@gmail.com>
2016-05-10 17:43:47 -0400

    refining id_handler

--- tools/id_handler.py
+++ tools/id_handler.py
@@ -1,8 +1,13 @@
 import tools.toolbox as toolbox
+import os.path
 from tools.JDict import JDict
 
 def get_unused_ID(jSettings):
-    IDs_json = "{}/IDs.Json".format(jSettings.saving["LoadedSave"])
+    IDs_json = "{}/IDs.json".format(jSettings.saving["LoadedSave"])
+    
+    if not os.path.isfile(IDs_json):
+        shutil.copyfile('saves/.defaults/IDs.json', IDs_json)
+    
     ID_settings = JDict( IDs_json, toolbox.parseJson( IDs_json ) )
     
     if len(ID_settings["UnusedIDs"]) > 1:
