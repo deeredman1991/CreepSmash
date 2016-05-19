2d480bf44e99a17c04304982b9c25f54758f84f5
deeredman1991 <deeredman1991@gmail.com>
2016-05-19 13:02:48 -0400

    started laying groundwork for player class

---
+++ objects/creatures/player/__init__.py
@@ -0,0 +1,7 @@
+#Placeholder class for player
+class Player(object):
+    def __init__(self):
+        self.maxHp = 250
+        self.hp = self.maxHp
+        self.maxArk = 1000
+        self.ark = maxArk
